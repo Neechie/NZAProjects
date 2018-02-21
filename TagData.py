14184e3ab3d0accd5c45f68b5406e72e64c30e28
Neechie <brendan_neech@hotmail.com>
2018-02-21 15:55:13 +1100

    Update TagData.py

--- TagData.py
+++ TagData.py
@@ -7,21 +7,10 @@
 #   Version : 1.0
 #
 
-#Tag Data array
-#TAG = [     "ff:04:19:c1:cf:2d",
-#            "ff:3a:60:93:de:a1",
-#            "c6:54:f4:99:e7:e7",
-#            "cf:c3:75:38:f2:ed"
-#           ]
 
-NAME = [
-           "Brendan",
-           "Danielle",
-           "Russell",
-           "Tile4"
-           ]
-TAG = [     ["Brendan","ff:04:19:c1:cf:2d"],
-            ["Danielle","ff:3a:60:93:de:a1"],
-            ["Russell","c6:54:f4:99:e7:e7"],
-            ["Tile4","cf:c3:75:38:f2:ed"]
+TAG = [     
+            ["Brendan","ff:04:19:c1:cf:2d",0],
+            ["Danielle","ff:3a:60:93:de:a1",0],
+            ["Russell","c6:54:f4:99:e7:e7",0],
+            ["Tile4","cf:c3:75:38:f2:ed",0]
            ]
